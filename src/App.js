5c13b81ae0dc8f3380cf28f981e22a045332d93f
wassem-qaneer <s12325968@stu.najah.edu>
2024-12-05 14:54:01 +0200

    edit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 
 import './App.css';
 import Section from './components/Section';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
 
 function App() {
 return (   <>
--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,59 +1,77 @@
 import Card from "./Card";
-import "./carousel.css"
+import "./carousel.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 const Carousel = () => {
-  return( <div
-    id="carouselExampleControlsNoTouching"
-    className="carousel slide"
-    data-bs-touch="false" 
-    data-bs-wrap="false" 
-  >
-    <div className="carousel-inner col-2 ">
+  return (
+    <div
+      id="carouselExampleControlsNoTouching"
+      className="carousel slide"
+      data-bs-touch="true"
+      data-bs-wrap="true"
+    >
       <div className="carousel-item active">
-        <Card name="card 1" />
+        <div className="d-flex justify-content-center">
+          <Card name="card 1" />
+          <Card name="card 2" />
+          <Card name="card 3" />
+          <Card name="card 4" />
+          <Card name="card 5" />
+        </div>
       </div>
+
       <div className="carousel-item">
-        <Card name="card 2" />
+        <div className="d-flex justify-content-center">
+          <Card name="card 6" />
+          <Card name="card 7" />
+          <Card name="card 8" />
+        </div>
       </div>
+
       <div className="carousel-item">
-        <Card name="card 3" />
+        <div className="d-flex justify-content-center">
+          <Card name="card 9" />
+          <Card name="card 10" />
+          <Card name="card 11" />
+        </div>
       </div>
+
       <div className="carousel-item">
-        <Card name="card 4" />
+        <div className="d-flex justify-content-center">
+          <Card name="card 12" />
+          <Card name="card 13" />
+          <Card name="card 14" />
+        </div>
       </div>
+
       <div className="carousel-item">
-        <Card name="card 5" />
+        <div className="d-flex justify-content-center">
+          <Card name="card 15" />
+          <Card name="card 16" />
+          <Card name="card 17" />
+        </div>
       </div>
-      <div className="carousel-item">
-        <Card name="card 6" />
-      </div>
+
+      <button
+        className="carousel-control-prev btn-dark"
+        type="button"
+        data-bs-target="#carouselExampleControlsNoTouching"
+        data-bs-slide="prev"
+      >
+        <span className="carousel-control-prev-icon" aria-hidden="false"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+
+      <button
+        className="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExampleControlsNoTouching"
+        data-bs-slide="next"
+      >
+        <span className="carousel-control-next-icon" aria-hidden="false"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
     </div>
-
-    <button
-      className="carousel-control-prev"
-      type="button"
-      data-bs-target="#carouselExampleControlsNoTouching"
-      data-bs-slide="prev"
-      style={{backgroundColor:"rgba(0,0,0,0)",borderRadius:"50%"}}
-    >
-      <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span className="visually-hidden">Previous</span>
-      
-
-    </button>
-
-    
-    <button
-      className="carousel-control-next"
-      type="button"
-      data-bs-target="#carouselExampleControlsNoTouching"
-      data-bs-slide="next"
-      style={{backgroundColor:"rgba(0,0,0,0)",borderRadius:"50%"}}
-
-    >
-      <span className="carousel-control-next-icon" aria-hidden="true"></span>
-      <span className="visually-hidden">Next</span>
-    </button>
-  </div>)
+  );
 };
 
 export default Carousel;
