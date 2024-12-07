e034c69c3f5bbce6cdd527bdfcdb44e6908f2519
dareen-2006 <s12323368@stu.najah.edu>
2024-12-07 09:12:36 +0200

    "done"

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,73 +1,63 @@
 import Card from "./Card";
 import "./carousel.css";
+import "./Card.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+
 const Carousel = () => {
+  
   return (
+    
     <div
       id="carouselExampleControlsNoTouching"
       className="carousel slide"
       data-bs-touch="true"
-      data-bs-wrap="true"
+      data-bs-wrap="false"
     >
-      <div className="carousel-item active">
-        <div className="d-flex justify-content-center">
-          <Card name="card 1" />
-          <Card name="card 2" />
-          <Card name="card 3" />
-          <Card name="card 4" />
-          <Card name="card 5" />
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <div className="d-flex flex-nowrap overflow-auto">
+            <div className="card-container">
+              <Card name="card 1" />
+            </div>
+            <div className="card-container">
+              <Card name="card 2" />
+            </div>
+            <div className="card-container">
+              <Card name="card 3" />
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className="carousel-item">
-        <div className="d-flex justify-content-center">
-          <Card name="card 6" />
-          <Card name="card 7" />
-          <Card name="card 8" />
-        </div>
-      </div>
-
-      <div className="carousel-item">
-        <div className="d-flex justify-content-center">
-          <Card name="card 9" />
-          <Card name="card 10" />
-          <Card name="card 11" />
-        </div>
-      </div>
-
-      <div className="carousel-item">
-        <div className="d-flex justify-content-center">
-          <Card name="card 12" />
-          <Card name="card 13" />
-          <Card name="card 14" />
-        </div>
-      </div>
-
-      <div className="carousel-item">
-        <div className="d-flex justify-content-center">
-          <Card name="card 15" />
-          <Card name="card 16" />
-          <Card name="card 17" />
+        <div className="carousel-item">
+          <div className="d-flex flex-nowrap overflow-auto">
+            <div className="card-container">
+              <Card name="card 4" />
+            </div>
+            <div className="card-container">
+              <Card name="card 5" />
+            </div>
+            <div className="card-container">
+              <Card name="card 6" />
+            </div>
+          </div>
         </div>
       </div>
 
       <button
-        className="carousel-control-prev btn-dark"
+        className="carousel-control-prev"
         type="button"
         data-bs-target="#carouselExampleControlsNoTouching"
         data-bs-slide="prev"
       >
-        <span className="carousel-control-prev-icon" aria-hidden="false"></span>
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
         <span className="visually-hidden">Previous</span>
       </button>
-
       <button
         className="carousel-control-next"
         type="button"
         data-bs-target="#carouselExampleControlsNoTouching"
         data-bs-slide="next"
       >
-        <span className="carousel-control-next-icon" aria-hidden="false"></span>
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
         <span className="visually-hidden">Next</span>
       </button>
     </div>
